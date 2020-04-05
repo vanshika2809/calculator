be3685c77b643e0d8af311214ba026c9d3550c60
vanshika2809 <vanshika.meh@gmail.com>
2020-04-05 20:15:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const numbers= document.querySelectorAll(".number")
+numbers.forEach((number) =>{
+	number.addEventListener("click",(event) => {
+		console.log(event.target.value)
+	})
+	
+
+})
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen =(number) => {
+	calculatorScreen.value=number
+}
+
+const numbers = document.querySelectorAll(".number")
+
+numbers.forEach((number)=>{
+	number.addEventListener("click",(event) =>{
+		updateScreen(event.target.value)
+	})
+})
